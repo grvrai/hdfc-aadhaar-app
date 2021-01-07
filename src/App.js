7f9a1fa15b4c117fbe4bb41f26d4ac638d2e9b46
Gaurav Rai <grv.rai@live.in>
2021-01-07 14:36:41 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 
 import Container from "@material-ui/core/Container";
 
@@ -20,11 +20,11 @@ import Constants from "./constants";
 import AuthService from "./services/auth-service";
 
 import {
-  // HashRouter as Router,
-  Switch,
-  Route,
-  useLocation,
-  withRouter,
+	// HashRouter as Router,
+	Switch,
+	Route,
+	useLocation,
+	withRouter,
 } from "react-router-dom";
 
 // import PrivateRoute from './routes/PrivateRoute'
@@ -34,162 +34,122 @@ import "./App.css";
 import ContactForm from "./pages/ContactPage/ContactForm";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // console.log(AuthService)
-    this.state = {
-      user: AuthService.getUserData(),
-      state: AuthService.getUserState(),
-      isLoggedIn: AuthService.isLoggedIn(),
-    };
+	constructor(props) {
+		super(props);
+		// console.log(AuthService)
+		this.state = {
+			user: AuthService.getUserData(),
+			state: AuthService.getUserState(),
+			isLoggedIn: AuthService.isLoggedIn(),
+		};
 
-    // console.log(this.state)
+		// console.log(this.state)
 
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
 
-    PeakPwa.webapp.registerServiceWorker();
-  }
+		PeakPwa.webapp.registerServiceWorker();
+	}
 
-  componentDidMount() {
-    document.addEventListener("PeakPwaInit", function () {
-      // console.log('PeakPwaInit');
-    //   if (!PeakPwa.webapp.installed) PeakPwa.subscribe();
-    });
+	componentDidMount() {
+		document.addEventListener("PeakPwaInit", function () {
+			// console.log('PeakPwaInit');
+			//   if (!PeakPwa.webapp.installed) PeakPwa.subscribe();
+		});
 
-    // if (this.state.isLoggedIn && this.state.state) {
+		// if (this.state.isLoggedIn && this.state.state) {
 
-    // }
-    PeakPwa.init(Constants, this.state.state);
-  }
+		// }
+		PeakPwa.init(Constants, this.state.state);
+	}
 
-  login(user_data, state_data) {
-    AuthService.login(user_data, state_data);
+	login(user_data, state_data) {
+		AuthService.login(user_data, state_data);
 
-    this.setState({
-      isLoggedIn: true,
-      user: user_data,
-      state: state_data,
-    });
-  }
+		this.setState({
+			isLoggedIn: true,
+			user: user_data,
+			state: state_data,
+		});
+	}
 
-  logout() {
-    // localStorage.clear();
-    AuthService.logout();
+	logout() {
+		// localStorage.clear();
+		AuthService.logout();
 
-    this.setState({
-      isLoggedIn: false,
-      user: null,
-      state: null,
-    });
+		this.setState({
+			isLoggedIn: false,
+			user: null,
+			state: null,
+		});
 
-    this.props.history.push("/");
-  }
+		this.props.history.push("/");
+	}
 
-  render() {
-    // console.log(this.state);
-    if (!this.state.isLoggedIn) {
-      return (
-        <Switch>
-          <Route
-            path="/"
-            // exact
-            render={(props) => (
-              <LoginPage onLoginSuccess={this.login} {...props} />
-            )}></Route>
-        </Switch>
-      );
-    } else {
-      return (
-        <div>
-          <MenuAppBar onLogout={this.logout} />
-          <ScrollToTop />
-          <Container
-            disableGutters
-            style={{ paddingTop: 56, paddingBottom: 56 }}>
-            <Switch>
-              <Route
-                path="/notifications"
-                render={(props) => (
-                  <NotificationContainer
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-              <Route
-                path="/customerdata"
-                render={(props) => (
-                  <CustomerDataPage
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-              <Route
-                path="/marketingactivity"
-                render={(props) => (
-                  <MarketingActivityPage
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-              <Route
-                path="/dailyactivity"
-                render={(props) => (
-                  <DailyActivityPage
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-              <Route
-                path="/issues"
-                render={(props) => (
-                  <IssuePage
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-								 <Route
-                path="/app_issue"
-                render={(props) => (
-                  <ContactForm
-                    {...props}
-                  />
-                )}></Route>
-              <Route
-                path="/change_password"
-                render={(props) => <ChangePasswordForm {...props} />}></Route>
-              <Route
-                path="/"
-                exact
-                render={(props) => (
-                  <HomeView
-                    state={this.state.state}
-                    user={this.state.user}
-                    {...props}
-                  />
-                )}></Route>
-            </Switch>
-          </Container>
-          <BottomNav />
-        </div>
-      );
-    }
-  }
+	render() {
+		// console.log(this.state);
+		if (!this.state.isLoggedIn) {
+			return (
+				<Switch>
+					<Route
+						path="/"
+						// exact
+						render={(props) => <LoginPage onLoginSuccess={this.login} {...props} />}></Route>
+				</Switch>
+			);
+		} else {
+			return (
+				<div>
+					<MenuAppBar onLogout={this.logout} />
+					<ScrollToTop />
+					<Container disableGutters style={{paddingTop: 56, paddingBottom: 56}}>
+						<Switch>
+							<Route
+								path="/notifications"
+								render={(props) => (
+									<NotificationContainer state={this.state.state} user={this.state.user} {...props} />
+								)}></Route>
+							<Route
+								path="/customerdata"
+								render={(props) => (
+									<CustomerDataPage state={this.state.state} user={this.state.user} {...props} />
+								)}></Route>
+							<Route
+								path="/marketingactivity"
+								render={(props) => (
+									<MarketingActivityPage state={this.state.state} user={this.state.user} {...props} />
+								)}></Route>
+							<Route
+								path="/dailyactivity"
+								render={(props) => (
+									<DailyActivityPage state={this.state.state} user={this.state.user} {...props} />
+								)}></Route>
+							<Route
+								path="/issues"
+								render={(props) => <IssuePage state={this.state.state} user={this.state.user} {...props} />}></Route>
+							<Route path="/app_issue" render={(props) => <ContactForm {...props} />}></Route>
+							<Route path="/change_password" render={(props) => <ChangePasswordForm {...props} />}></Route>
+							<Route
+								path="/"
+								exact
+								render={(props) => <HomeView state={this.state.state} user={this.state.user} {...props} />}></Route>
+						</Switch>
+					</Container>
+					<BottomNav />
+				</div>
+			);
+		}
+	}
 }
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+	const {pathname} = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-  return null;
+	return null;
 }
 
 export default withRouter(App);
